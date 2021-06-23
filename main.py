852a62974aa54e7271a7a15ad8c7f46184c454fb
Nargis <nargis.salim@btinternet.com>
2021-06-24 00:30:07 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Fullfilment Agency
+# Program to help minimise costs when delivering goods to consumers
+# Use functions which can receive a variable number of keywords and positional arguments.
+# Working with tests
+
+from fullfillment_agency import get_distance, format_price, SHIPPING_PRICES
+from test import test_function
+
+# Define calculate_shipping_cost()
+
+
+# Test the function by calling 
+# test_function(calculate_shipping_cost)
+
+# Define calculate_driver_cost() 
+
+
+# Test the function by calling 
+# test_function(calculate_driver_cost)
+
+# Define calculate_money_made() 
+
+
+# Test the function by calling 
+# test_function(calculate_money_made)
